571fe6e10197d49d1ef43a4367c45adc809b0f60
Faizudheen M <37646292+Faizudheen@users.noreply.github.com>
2024-08-07 13:14:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function AppContent() {
 
         {quizState === 'landing' ? (
            <div className=" max-w-lg mx-auto mt-8 p-6 bg-white rounded-lg shadow-lg filter backdrop-blur-lg bg-opacity-10 flex flex-col gap-3 justify-center items-center">
-           <h1 className='text-xl font-bold text-white'>LET'S YOUR KNOWLEDGE !!!</h1>
+           <h1 className='text-xl font-bold text-white'>LET'S FIND YOUR KNOWLEDGE !!!</h1>
            <button className='bg-blue-600/55 text-white hover:bg-blue-700 p-3 rounded-md transition-colors duration-300 '
            onClick={()=>setQuizState('playing')}>
              START QUIZ
